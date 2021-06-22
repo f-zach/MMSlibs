0b6ed3ab77a09b9a297c75d76c942c73c97515ac
f-zach <63615390+f-zach@users.noreply.github.com>
2021-06-22 11:19:35 +0200

    first commit RTD library

---
+++ RTD/src/RTD.cpp
@@ -0,0 +1,72 @@
+#include "RTD.h"
+
+RTDmodule::RTDmodule(int i2c_addressRTD)
+    : MAX31865module(0)
+{
+    _i2c_addressRTD = i2c_addressRTD;
+}
+
+void RTDmodule::config(byte sensorPosition, int Rref, int mode, int wiring = 1, int FaultDetection = 0, int filter = 1)
+{
+    _sensorPositions = sensorPosition;
+
+    _sensorCount = 0
+
+    MAX31865rtd.config(Rref, mode, wiring, FaultDetection, filter);
+
+    for(_i = 0; _i < 4; _i++)
+    {
+        if(bitRead(_sensorPositions, _i))
+        {
+            _sensorCount++;
+        }
+    }
+
+    Wire.beginTransmission(_i2c_addressTCO);
+    Wire.write(0xFF);
+    Wire.endTransmission();
+}
+
+void RTDmodule::startMeasurementSingle(int sensorNo)
+{
+    Wire.beginTransmission(_i2c_addressTCO);
+    Wire.write(_sensorCS[sensorNo]);
+    Wire.endTransmission();
+
+    MAX31865rtd.startMeasurement();
+
+    Wire.beginTransmission(_i2c_addressTCO);
+    Wire.write(0xFF);
+    Wire.endTransmission();
+}
+
+void RTDmodule::startMesasurementAll()
+{
+    for(_i = 0, _i < _sensorCount, _i++)
+    {
+        MAX31865rtd.startMesasurementSingle(_i);
+    }
+}
+
+float RTDmodule::readTemperatureSingle(int sensorNo)
+{
+    Wire.beginTransmission(_i2c_addressTCO);
+    Wire.write(_sensorCS[sensorNo]);
+    Wire.endTransmission();
+
+    TemperatureSingle = MAX31865rtd.read();
+
+    Wire.beginTransmission(_i2c_addressTCO);
+    Wire.write(0xFF);
+    Wire.endTransmission();
+
+    return TemperatureSingle;
+}
+
+void RTDmodule::readTemperatureAll()
+{
+    for(_i = 0, _i < _sensorCount, _i++)
+    {
+       TemperatureAll[_i] = readTemperatureSingle(_i);
+    }
+}
---
+++ RTD/src/RTD.h
@@ -0,0 +1,30 @@
+#ifndef RTD_MOD
+#define RTD_MOD
+
+#include <Arduino.h>
+#include <max31865_no_delay.h>
+#include <SPI.h>
+
+#define RREF 430.0
+
+class RTDmodule
+{
+private:
+   int _i2c_addressRTD;
+   byte _sensorPositions;
+   int _sensorCount;
+   int _i;
+   byte _sensorCS[4] = {B00000001, B00000010, B00000100, B00001000};
+
+public:
+    RTDmodule(int i2c_addressRTD);
+    void config(byte sensorPosition, int Rref, int mode, int wiring = 1, int FaultDetection = 0, int filter = 1);
+    void startMeasurementSingle(int sensorNo);
+    void startMesasurementAll();
+    float readTemperatureSingle(int sensorNo);
+    void readTemperatureAll();
+    int TemperatureSingle;
+    int TemperatureAll[4];
+    MAX31865 MAX31865rtd;
+};
+
